8eb237a5a87c215ac403d8306af431fa00ad08e1
Rustam Ismaylov <ismaylovrx@gmail.com>
2017-10-27 09:45:30 +0400

    Создана функция которая вычисляет расстояние между двумя точками. Final Version.

--- sandbox/src/main/java/rustam/java_qa_course/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/rustam/java_qa_course/sandbox/MyFirstProgram.java
@@ -5,17 +5,13 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
 
-        Point p1 = new Point(15.0, 120.0);
+        Point p1 = new Point(15.0, 10.0);
         Point p2 = new Point(16.0, 20.0);
 
-        System.out.println("Расстояние между двумя точками  координат = " + Distance(p1, p2));
+        System.out.println("Расстояние между двумя точками  координат = " + p1.distance(p2));
     }
 
-    public static double Distance (Point p1, Point p2){
 
-
-        return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y)));
-    }
 
 
 
