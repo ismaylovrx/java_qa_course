f755649fe424b7b25cdd45c26862de343d13b699
Rustam Ismaylov <ismaylovrx@gmail.com>
2017-10-29 20:09:35 +0400

    TestNG_Lesson_2.4_Refactoring_NewUser
    TestNG_Lesson_2.4_Refactoring_NewUser

---
+++ addressbook-web-tests/src/test/java/rustam/addressbook_HW1/NewUserCreation.java
@@ -0,0 +1,129 @@
+package com.example.tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import rustam.addressbook_HW1.NewUserData;
+
+public class NewUserCreation {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    baseUrl = "http://localhost:8080";
+    driver = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    login("admin", "secret");
+  }
+
+
+  @Test
+  public void testNewUserCreation() throws Exception {
+
+    gotoNewUserPage();
+    initNewUserCreation();
+    fillNewUserForm(new NewUserData("Rafig", "Suleyman", "Boxter", "Tiesto Company", "Zarfarpalan 133", "5947957", "0703442020", "5468123", "test@test.com", "23", "November", "1980"));
+    submitNewUserCreation();
+
+  }
+
+  private void Logout() {
+    driver.findElement(By.linkText("Logout")).click();
+  }
+
+  private void submitNewUserCreation() {
+    driver.findElement(By.xpath("//input[21]")).click();
+  }
+
+  private void fillNewUserForm(NewUserData newUserData) {
+    driver.findElement(By.name("firstname")).clear();
+    driver.findElement(By.name("firstname")).sendKeys(newUserData.getName());
+    driver.findElement(By.name("lastname")).clear();
+    driver.findElement(By.name("lastname")).sendKeys(newUserData.getLastname());
+    driver.findElement(By.name("nickname")).clear();
+    driver.findElement(By.name("nickname")).sendKeys(newUserData.getNickname());
+    driver.findElement(By.name("company")).clear();
+    driver.findElement(By.name("company")).sendKeys(newUserData.getCompany());
+    driver.findElement(By.name("address")).clear();
+    driver.findElement(By.name("address")).sendKeys(newUserData.getAddress());
+    driver.findElement(By.name("home")).clear();
+    driver.findElement(By.name("home")).sendKeys(newUserData.getHome_tel());
+    driver.findElement(By.name("mobile")).clear();
+    driver.findElement(By.name("mobile")).sendKeys(newUserData.getMobile_tel());
+    driver.findElement(By.name("work")).clear();
+    driver.findElement(By.name("work")).sendKeys(newUserData.getWork_tel());
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys(newUserData.getEmail());
+    new Select(driver.findElement(By.name("bday"))).selectByVisibleText(newUserData.getBday());
+    new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText(newUserData.getBmonth());
+    driver.findElement(By.name("byear")).clear();
+    driver.findElement(By.name("byear")).sendKeys(newUserData.getByear());
+  }
+
+  private void initNewUserCreation() {
+    driver.findElement(By.linkText("add new")).click();
+  }
+
+  private void gotoNewUserPage() {
+    driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+  }
+
+  private void login(String username, String password) {
+    driver.get(baseUrl + "/addressbook/");
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys(username);
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys(password);
+  }
+
+  @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+    Logout();
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
---
+++ addressbook-web-tests/src/test/java/rustam/addressbook_HW1/NewUserData.java
@@ -0,0 +1,79 @@
+package rustam.addressbook_HW1;
+
+public class NewUserData {
+    private final String name;
+    private final String lastname;
+    private final String nickname;
+    private final String company;
+    private final String address;
+    private final String home_tel;
+    private final String mobile_tel;
+    private final String work_tel;
+    private final String email;
+    private final String bday;
+    private final String bmonth;
+    private final String byear;
+
+    public NewUserData(String name, String lastname, String nickname, String company, String address, String home_tel, String mobile_tel, String work_tel, String email, String bday, String bmonth, String byear) {
+        this.name = name;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.company = company;
+        this.address = address;
+        this.home_tel = home_tel;
+        this.mobile_tel = mobile_tel;
+        this.work_tel = work_tel;
+        this.email = email;
+        this.bday = bday;
+        this.bmonth = bmonth;
+        this.byear = byear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHome_tel() {
+        return home_tel;
+    }
+
+    public String getMobile_tel() {
+        return mobile_tel;
+    }
+
+    public String getWork_tel() {
+        return work_tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getBday() {
+        return bday;
+    }
+
+    public String getBmonth() {
+        return bmonth;
+    }
+
+    public String getByear() {
+        return byear;
+    }
+}
