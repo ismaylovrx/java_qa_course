ec342eb7ed80bc4e519080e2a9e39f3e288d7ec3
Rustam Ismaylov <ismaylovrx@gmail.com>
2017-11-02 20:36:32 +0400

    Start_HW_#7

--- addressbook-web-tests/src/test/java/rustam/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/rustam/addressbook/appmanager/ApplicationManager.java
@@ -1,24 +1,42 @@
 package rustam.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
 
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.fail;
 
 public class ApplicationManager {
-    public WebDriver driver;
+    WebDriver driver;
 
     private UserHelper userHelper;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupsHelper groupsHelper;
     public StringBuffer verificationErrors = new StringBuffer();
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        driver = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        System.setProperty("webdriver.chrome.driver", "D:\\selenium\\chromedriver.exe");
+        System.setProperty("webdriver.ie.driver", "D:\\selenium\\IEDriverServer.exe");
+        if (browser.equals (BrowserType.FIREFOX)) {
+            driver = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        }   else if (browser.equals(BrowserType.CHROME)) {
+            driver = new ChromeDriver();
+        }   else if (browser.equals(BrowserType.IE)) {
+            driver = new InternetExplorerDriver();
+        }
+
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("http://localhost:8080/addressbook/");
         groupsHelper = new GroupsHelper(driver);
--- addressbook-web-tests/src/test/java/rustam/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/rustam/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package rustam.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import rustam.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    public final ApplicationManager app = new ApplicationManager();
+    public final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
